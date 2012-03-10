a96c0bb1b66b176ace83c0425ad6899436b09193
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2012-03-10 01:41:01 +0000

    Hydrogen: Updated modules tool.

--- trunk/tools/Hydrogen/modules/classes/Controller.php
+++ trunk/tools/Hydrogen/modules/classes/Controller.php
@@ -13,12 +13,73 @@ class Controller {
 	protected function addData( $key, $value ){
 		$this->view->addData( $key, $value );
 	}
+	
+	public function details( $moduleId ){
+		$model		= new Model( $this->env );
+		$module		= $model->get( $moduleId );
+		$module->neededModules		= $model->getAllNeededModules( $moduleId );
+		$module->supportedModules	= $model->getAllSupportedModules( $moduleId );
+		$this->addData( 'module', $module );
+		$this->addData( 'moduleId', $moduleId );
+	}
 
-	public function copy( $moduleId ){
-		if( $this->logic->installModule( $moduleId, Logic::INSTALL_TYPE_COPY ) )
-			$this->messenger->noteSuccess( $this->words['msg']['moduleCopied'], $moduleId );
+	public function getView(){
+		return $this->view;
+	}
+
+	public function index( $moduleId = NULL ){
+		$model	= new Model( $this->env );
+		$this->addData( 'modules', $model->getAll() );
+/*		$this->addData( 'modulesAvailable', $model->getAvailable() );
+		$this->addData( 'modulesInstalled', $model->getInstalled() );
+		$this->addData( 'modulesNotInstalled', $model->getNotInstalled() );
+*/	}
+
+	public function install( $moduleId ){
+		$request	= $this->env->getRequest();
+		$model		= new Model( $this->env );
+		$module		= $model->get( $moduleId );
+		$module->neededModules		= $model->getAllNeededModules( $moduleId );
+		$module->supportedModules	= $model->getAllSupportedModules( $moduleId );
+		if( $request->get( 'doInstall' ) ){
+			if( $request->get( 'type' ) == 'copy' ){
+				if( $this->logic->installModule( $moduleId, Logic::INSTALL_TYPE_COPY, TRUE ) )
+					$this->messenger->noteSuccess( $this->words['msg']['moduleCopied'], $moduleId );
+				else
+					$this->messenger->noteError( $this->words['msg']['moduleNotCopied'], $moduleId );
+				$this->restart( array( 'action' => 'details', 'moduleId' => $moduleId ) );
+			}
+			else if( $request->get( 'type' ) == 'link' ){
+				if( $this->logic->installModule( $moduleId, Logic::INSTALL_TYPE_LINK, TRUE ) )
+					$this->messenger->noteSuccess( $this->words['msg']['moduleLinked'], $moduleId );
+				else
+					$this->messenger->noteError( $this->words['msg']['moduleNotLinked'], $moduleId );
+				$this->restart( array( 'action' => 'details', 'moduleId' => $moduleId ) );
+			}
+		}
+		$this->addData( 'module', $module );
+		$this->addData( 'moduleId', $moduleId );
+	}
+
+	protected function restart( $parameters = array() ){
+		if( is_array( $parameters ) )
+			$parameters	= http_build_query( $parameters, '', '&' );
+		if( is_string( $parameters ) ){
+			$parameters	= strlen( $parameters ) ? '?'.$parameters : '';
+			header( 'Location: ./'.$parameters );
+			exit;
+		}
+	}
+
+	protected function setData( $data, $topic = NULL ){
+		$this->view->setData( $data, $topic );
+	}
+
+	public function uninstall( $moduleId, $verbose = TRUE ){
+		if( $this->logic->uninstallModule( $moduleId, $verbose ) )
+			$this->messenger->noteSuccess( $this->words['msg']['moduleUninstalled'], $moduleId );
 		else
-			$this->messenger->noteError( $this->words['msg']['moduleNotCopied'], $moduleId );
+			$this->messenger->noteError( $this->words['msg']['moduleNotUninstalled'], $moduleId );
 		$this->restart( array( 'action' => 'details', 'moduleId' => $moduleId ) );
 	}
 
@@ -60,54 +121,6 @@ class Controller {
 		$page->addBody( $body );
 		print( $page->build( array( 'style' => 'margin: 1em' ) ) );
 		exit;
-	}
-	
-	public function details( $moduleId ){
-		$model	= new Model( $this->env );
-		$this->addData( 'module', $model->get( $moduleId ) );
-		$this->addData( 'moduleId', $moduleId );
-	}
-
-	public function getView(){
-		return $this->view;
-	}
-
-	public function index( $moduleId = NULL ){
-		$model	= new Model( $this->env );
-		$this->addData( 'modules', $model->getAll() );
-/*		$this->addData( 'modulesAvailable', $model->getAvailable() );
-		$this->addData( 'modulesInstalled', $model->getInstalled() );
-		$this->addData( 'modulesNotInstalled', $model->getNotInstalled() );
-*/	}
-
-	public function link( $moduleId ){
-		if( $this->logic->installModule( $moduleId, Logic::INSTALL_TYPE_LINK ) )
-			$this->messenger->noteSuccess( $this->words['msg']['moduleLinked'], $moduleId );
-		else
-			$this->messenger->noteError( $this->words['msg']['moduleNotLinked'], $moduleId );
-		$this->restart( array( 'action' => 'details', 'moduleId' => $moduleId ) );
-	}
-
-	protected function restart( $parameters = array() ){
-		if( is_array( $parameters ) )
-			$parameters	= http_build_query( $parameters, '', '&' );
-		if( is_string( $parameters ) ){
-			$parameters	= strlen( $parameters ) ? '?'.$parameters : '';
-			header( 'Location: ./'.$parameters );
-			exit;
-		}
-	}
-
-	protected function setData( $data, $topic = NULL ){
-		$this->view->setData( $data, $topic );
-	}
-
-	public function uninstall( $moduleId, $verbose = TRUE ){
-		if( $this->logic->uninstallModule( $moduleId, $verbose ) )
-			$this->messenger->noteSuccess( $this->words['msg']['moduleUninstalled'], $moduleId );
-		else
-			$this->messenger->noteError( $this->words['msg']['moduleNotUninstalled'], $moduleId );
-		$this->restart( array( 'action' => 'details', 'moduleId' => $moduleId ) );
 	}
 }
 ?>
