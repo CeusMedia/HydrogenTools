1e1da015192d028ca60765ab941f34f580e1962a
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2012-03-20 02:51:22 +0000

    Hydrogen: Updated ModuleManager.

--- trunk/tools/Hydrogen/modules/templates/view.config.php
+++ trunk/tools/Hydrogen/modules/templates/view.config.php
@@ -4,12 +4,50 @@ $count	= 0;
 $config	= '-';
 if( $module->config ){
 	$config	= array();
-	foreach( $module->config as $key => $value ){
+	foreach( $module->config as $item ){
 		$count++;
-		$config[]	= UI_HTML_Tag::create( 'dt', $key ).UI_HTML_Tag::create( 'dd', $value );
+		$dt		= UI_HTML_Tag::create( 'dt', $item->key );
+		$dd		= UI_HTML_Tag::create( 'dd', $item->value, array( 'rel' => $item->type ) );
+		$config[]	= $dt.$dd;
 	}
 	$config	= UI_HTML_Tag::create( 'dl', join( $config ) );
 }
 
-return $config.'<div class="clearfix"></div>';
+return $config.'<div class="clearfix"></div>
+<script>
+
+$(document).ready(function(){
+	$("dd").each(function(){
+		$(this).click(function(){
+			var dd = $(this);
+			if(!dd.data("state")){
+				dd.data("key",dd.prev().html());
+				dd.data("type",dd.attr("rel"));
+				dd.data("value",dd.html());
+				dd.data("state",1);
+			}
+			if(dd.data("state") == 1){
+				$("dd>input").each(function(){
+					var dd = $(this).parent();
+					dd.html(dd.data("value"));
+				});
+				if(dd.data("type") == "string")
+					var input = $("<input/>").val(dd.data("value")).attr("type","text");
+				else if(type == "boolean"){
+					var input = $("<input/>").attr("type","checkbox");
+					var checked = value == "yes" || value == "1";
+					input.attr("checked",checked ? "checked" : "");
+					input.val(1).attr("name",key).data("value",value);
+				}
+				$(this).html(input);
+				dd.data("state",2);
+			}
+			else if(!dd.data("state") == 2){
+				dd.data("state",1);
+			}
+		});
+	});
+});
+</script>
+';
 ?>
