9aeacfb0e6ed72eb476e491a5446bf5b26a093ba
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2013-06-11 02:01:31 +0000

    Tools/Hydrogen: Updated JsError for better server integration.

--- trunk/tools/Hydrogen/JsError/index.php
+++ trunk/tools/Hydrogen/JsError/index.php
@@ -4,10 +4,10 @@ require_once 'cmClasses/trunk/autoload.php5';
 $pathApp	= '/projects/Chat/client/';
 $pathRoot	= getEnv( 'DOCUMENT_ROOT' );
 
-
 CMC_Loader::registerNew( 'php5', 'JsError_', 'classes/' );
 $fileConfig	= 'config.ini';
 
+chdir( dirname( __FILE__ ) );
 $d	= new JsError_Dispatcher();
 ?>
 
