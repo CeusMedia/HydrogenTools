b9d9c7a080c4f97d18b3faa5cc1f8ed0656750e4
christian.wuerker <christian.wuerker@e4149dc6-e81b-11de-b362-3513cf04e977>
2013-12-20 21:19:43 +0000

    Tools/Hydrogen: Changed Setup configuration from INI to JSON.

--- trunk/tools/Hydrogen/Setup/templates/index/index.instances.php
+++ trunk/tools/Hydrogen/Setup/templates/index/index.instances.php
@@ -1,16 +1,15 @@
 <?php
 $list		= array();
-foreach( $instances as $entry ){
-#	print_m( $entry );
+foreach( $instances as $id => $entry ){
 	if( empty( $entry->configPath ) )
 		$entry->configPath	= 'config/';
 	if( empty( $entry->configFile ) )
 		$entry->configFile	= 'config.ini';
 	$configFile	= $entry->uri.$entry->configPath.$entry->configFile;
 	$class	= array( file_exists( $configFile ) ? 'check-okay' : 'check-fail' );
-	if( $instanceId == $entry->id )
+	if( $instanceId == $id )
 		$class[]	= 'active';
-	$link	= UI_HTML_Elements::Link( './?selectInstanceId='.$entry->id, $entry->title );
+	$link	= UI_HTML_Elements::Link( './admin/instance/select/'.$id, $entry->title );
 	$list[$entry->title]	= UI_HTML_Tag::create( 'li', $link, array( 'class' => join( ' ', $class ) ) );
 }
 ksort( $list );
